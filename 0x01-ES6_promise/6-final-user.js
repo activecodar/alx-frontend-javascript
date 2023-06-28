573019ef027d5affb841636c78d0a470148215b0
activecodar <activecodar@gmail.com>
2023-06-28 22:20:08 +0300

    handle promises in bulk

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = (firstName, lastName, fileName) => Promise.allSettled([
-  signUpUser(firstName, lastName),
-  uploadPhoto(fileName),
-]).then((values) => {
-  const arr = [];
-  for (const item of values) {
-    arr.push({ status: item.status, value: item.value || item.reason });
-  }
-  return arr;
-});
-
-export default handleProfileSignup;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
+  });
+}
