428cf3f85a308d83e9ff591c45340255b2d9c821
activecodar <activecodar@gmail.com>
2023-06-28 21:03:12 +0300

    handle a promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: 'Successfully!' });
+      resolve({ status: 200, body: 'Success' });
     } else {
-      reject(new Error('An error occurred!'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
