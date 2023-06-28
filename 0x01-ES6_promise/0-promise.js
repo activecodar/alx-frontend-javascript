a7f7201f11ddd0952fbe44c774339152de55227f
activecodar <activecodar@gmail.com>
2023-06-28 20:53:25 +0300

    add promise check

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // API logic here...
+      // Resolve or reject based on API response
+    });
+  }
+  
