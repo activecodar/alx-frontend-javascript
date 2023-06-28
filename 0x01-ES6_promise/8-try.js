d4302b33ed8d48d2fa2c12c54d31c555a561a743
activecodar <activecodar@gmail.com>
2023-06-28 22:33:30 +0300

    handle division

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,8 @@
 const divideFunction = (numerator, denominator) => {
   if (denominator === 0) {
     throw new Error('Cannot divide by 0');
+  } else {
+    return numerator / denominator;
   }
-  return numerator / denominator;
 };
 export default divideFunction;
