d68ad867ab35c6fc5c83702a2646a71ca71d7418
activecodar <activecodar@gmail.com>
2023-06-29 19:10:08 +0300

    implment class inheritance
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from './5-building';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate the ${this.floors} floors slowly`;
+  }
+}
+
+export default SkyHighBuilding;
