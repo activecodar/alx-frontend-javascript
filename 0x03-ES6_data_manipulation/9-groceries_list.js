5f9513db6c661fcea92aebda5b99e7b41d023b84
activecodar <activecodar@gmail.com>
2023-07-04 19:14:29 +0300

    create a map
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+const groceriesList = () => {
+  const grocetriesMap = new Map();
+  grocetriesMap.set('Apples', 10);
+  grocetriesMap.set('Tomatoes', 10);
+  grocetriesMap.set('Pasta', 1);
+  grocetriesMap.set('Rice', 1);
+  grocetriesMap.set('Banana', 5);
+  return grocetriesMap;
+};
+
+export default groceriesList;
