2fc4188bca1e7cc308cc6a62f54afff969231a7e
activecodar <activecodar@gmail.com>
2023-07-05 19:46:27 +0300

    Brand convention & Nominal typing
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,23 @@
+interface MajorCredits {
+  credits: number;
+  brand: "major";
+}
+
+interface MinorCredits {
+  credits: number;
+  brand: "minor";
+}
+
+const sumMajorCredits = (subject1: MajorCredits, subject2: MajorCredits): MajorCredits => {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "major",
+  };
+}
+
+const sumMinorCredits = (subject1: MinorCredits, subject2: MinorCredits): MinorCredits => {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "minor",
+  };
+}
